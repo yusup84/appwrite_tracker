d7fa29959c31fad0d9477ae18b8798780544ce02
yusup84 <globalinformatika84@gmail.com>
2024-10-03 07:59:48 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# appwrite_tracker
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/dev/payu/appwrite_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.payu.appwrite_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
