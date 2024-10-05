411d0461969dda1bd386d16a70cf3644b325d468
yusup84 <globalinformatika84@gmail.com>
2024-10-05 08:37:26 +0700

    authentication state provider

---
+++ lib/auth_notifier/src/auth_state.dart
@@ -0,0 +1,29 @@
+
+
+import 'package:appwrite_tracker/auth_notifier/src/user.dart';
+
+class AuthState {
+  final User? user;
+  final AuthStatus status;
+  final String? error;
+
+  AuthState({
+    this.user,
+    this.error,
+    required this.status,
+  });
+
+  AuthState copyWith({
+    User? user,
+    AuthStatus? status,
+    String? error,
+  }) {
+    return AuthState(
+      user: user ?? this.user,
+      status: status ?? this.status,
+      error: error ?? this.error,
+    );
+  }
+}
+
+enum AuthStatus { loading, authenticating, authenticated, unauthenticated }
